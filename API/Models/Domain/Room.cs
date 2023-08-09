372f9fb896ef2a26dc034b145e54578095def007
Junnio <30598484+Junn1o@users.noreply.github.com>
2023-08-09 12:46:57 +0700

    Code cho model\domain phần Room và Room_Category. Room_Category đang đợi Category để thêm field cuối trước khi dùng enity framework

---
+++ API/Models/Domain/Room.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.Domain
+{
+    public class Room
+    {
+        [Key]
+        public int Id { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+        public int authorId { get; set; }
+        public string address { get; set; }
+    }
+}
---
+++ API/Models/Domain/Room_Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace API.Models.Domain
+{
+    public class Room_Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public int categoryId { get; set; }
+        public int roomId { get; set; }
+        public Room room { get; set; }
+    }
+}
