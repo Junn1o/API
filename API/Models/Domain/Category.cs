be4c259caaf11a15302bf6f448132d6da84cd143
Junnio <30598484+Junn1o@users.noreply.github.com>
2023-08-09 22:06:30 +0700

    e

--- API/Models/Domain/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace API.Models.Domain
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
