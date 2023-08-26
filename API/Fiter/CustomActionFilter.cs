48905af642c631270a8501c3c82a0f52b0497379
Huynh <112002caohuynh@gmail.com>
2023-08-26 07:55:45 +0700

    SQL

---
+++ API/Fiter/CustomActionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Fiter
+{
+    public class CustomActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid == false)
+            {
+                context.Result = new BadRequestResult();
+            }
+        }
+    }
+}
